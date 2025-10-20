47663db6e7af4605d8cdca47aabcaec4c36e5c38
hwanghj09 <hwanghj09@naver.com>
2025-10-20 23:39:08 +0900

    fundamental example code

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Home from './pages/Home'
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter >
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
