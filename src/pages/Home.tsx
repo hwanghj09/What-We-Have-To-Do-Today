a8536f1a7301ccfaa7bd31991d07576e64480f78
hwanghj09 <hwanghj09@naver.com>
2025-10-21 00:29:43 +0900

    add list

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,62 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import Cookies from 'js-cookie'
+
+interface Todo {
+  id: number
+  text: string
+}
 
 function Home() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState<Todo[]>([])
+  const [text, setText] = useState('')
+
+  useEffect(() => {
+    const savedTodos = Cookies.get('todos')
+    if (savedTodos) {
+      setTodos(JSON.parse(savedTodos))
+    }
+  }, [])
+
+  useEffect(() => {
+    Cookies.set('todos', JSON.stringify(todos))
+  }, [todos])
+
+  const addTodo = () => {
+    if (text.trim() !== '') {
+      const newTodo: Todo = {
+        id: Date.now(),
+        text: text,
+      }
+      setTodos([...todos, newTodo])
+      setText('')
+    }
+  }
+
+  const removeTodo = (id: number) => {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
 
   return (
     <>
-      <h1>Home Page</h1>
-      <p>Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <h1>Todo List</h1>
+      <div className='todolist'>
+        {todos.map(todo => (
+          <div key={todo.id}>
+            <input type="checkbox" />
+            <span>{todo.text}</span>
+            <button onClick={() => removeTodo(todo.id)}>Remove</button>
+          </div>
+        ))}
+      </div>
+      <input
+        type="text"
+        placeholder="Type something"
+        className='test'
+        value={text}
+        onChange={e => setText(e.currentTarget.value)}
+        onKeyPress={e => e.key === 'Enter' && addTodo()}
+      />
+      <button onClick={addTodo}>Add Todo</button>
     </>
   )
 }
